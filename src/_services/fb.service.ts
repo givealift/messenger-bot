3a02912a5dc437ab4c834cead527f6b9a8e58aae
Bartek Grodek <bartgrdk@gmail.com>
2018-05-13 16:28:50 +0200

    add sending message outside 24+1 FB window

--- src/_services/fb.service.ts
+++ src/_services/fb.service.ts
@@ -3,23 +3,43 @@ import { Http } from '../http';
 export class FacebookService {
     private readonly PAGE_ACCESS_TOKEN = process.env.BOT_PAGE_ACCESS_TOKEN;
     private readonly URI = "https://graph.facebook.com/v2.11/me/messages";
+    private readonly accessTokenParams = { "access_token": this.PAGE_ACCESS_TOKEN };
     private http = new Http();
 
-    async sendResponse(sender_psid: string, response: any) {
-        const request_body = this.constructRequestBody(sender_psid, response);
 
+    private async send(request_body: any) {
         try {
-            const body = await this.http.post(this.URI, request_body, { params: { "access_token": this.PAGE_ACCESS_TOKEN } })
+            const body = await this.http.post(this.URI, request_body, { params: this.accessTokenParams })
             console.log('message sent!')
             console.log(body);
         } catch (error) {
             console.error("Unable to send message: " + error);
         }
     }
+    async sendResponse(sender_psid: string, response: any) {
+        const request_body = this.constructResponseBody(sender_psid, response);
+        await this.send(request_body);
+    }
 
-    private constructRequestBody(sender_psid: string, response: any) {
+    async sendNotificaiton(sender_psid: string, response: any) {
+        const request_body = this.constructPairingBody(sender_psid, response);
+        await this.send(request_body);
+    }
+
+    private constructResponseBody(sender_psid: string, response: any) {
         return {
             "messaging_type": "RESPONSE",
+            "recipient": {
+                "id": sender_psid
+            },
+            "message": response
+        };
+    }
+
+    private constructPairingBody(sender_psid: string, response: any) {
+        return {
+            "messaging_type": "MESSAGE_TAG",
+            "tag": "PAIRING_UPDATE",
             "recipient": {
                 "id": sender_psid
             },
