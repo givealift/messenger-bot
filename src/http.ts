0012e00c67b9f93f9bb6a6e4ec2dc1726f503a83
Bartek Grodek <bartgrdk@gmail.com>
2018-05-13 14:52:36 +0200

    add logging to http

--- src/http.ts
+++ src/http.ts
@@ -12,7 +12,16 @@ export class Http {
     }
 
     public async get<T>(url: string, params?: { [key: string]: any }) {
+        console.log(`HTTP > ${url}`);
+        console.log(params);
         const response = await this._httpClient.get<T>(url, { params: params });
+        console.log(`HTTP <`);
+        if (response.data instanceof Array && response.data.length > 5) {
+            console.log(response.data.slice(0,5));
+            console.log("...");
+        } else {
+            console.log(response.data);
+        }
         return response.data;
     }
 
