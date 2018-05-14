ab1d32d3e9987bef85c0653615753fe66ca657e3
Bartek Grodek <bartgrdk@gmail.com>
2018-05-14 19:31:41 +0200

    route announcement & subscribed route info templates

--- src/response-builder.ts
+++ src/response-builder.ts
@@ -29,6 +29,44 @@ class ResponseBuilder {
     public list() {
         return new ListTemplateBuilder();
     }
+
+    public routeAnnouncment(from: string, to: string) {
+        return {
+            "dynamic_text": {
+                "text": `Hej, {{first_name}}! Pojawił się przejazd na trasie ${from} - ${to}`,
+                "fallback_text": `Hej! Pojawił się przejazd na trasie ${from} - ${to}`
+            }
+        }
+    }
+
+    public subscribedRoute(route: Route) {
+        return {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [
+                        {
+                            "title": `${route.from.city.name} - ${route.to.city.name}, ${moment(route.from.date).format("LL")}`,
+                            "subtitle": `${route.galUserPublicResponse.firstName} ${route.galUserPublicResponse.lastName}`,
+                            "buttons": [
+                                {
+                                    "title": "Szczegóły",
+                                    "type": "web_url",
+                                    "url": `https://givealift.herokuapp.com/route/${route.routeId}`
+                                },
+                                {
+                                    "title": "Zadzwoń",
+                                    "type": "phone_number",
+                                    "payload": route.galUserPublicResponse.phone
+                                }
+                            ]
+                        }
+                    ],
+                }
+            }
+        }
+    }
 }
 
 export const responseBuilder = new ResponseBuilder();
